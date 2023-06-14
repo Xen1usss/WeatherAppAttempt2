82cba8f7926f21b478d54fe9a25abbe88626de34
Ksenia <ksyshishe@mail.ru>
2023-06-14 17:02:23 +0300

    commit after lesson 10 with new mistake

--- app/src/main/java/fragments/HoursFragment.kt
+++ app/src/main/java/fragments/HoursFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import ru.startandroid.develop.weatherappattempt2.R
 import ru.startandroid.develop.weatherappattempt2.adapters.WeatherAdapter
+import ru.startandroid.develop.weatherappattempt2.adapters.WeatherModel
 import ru.startandroid.develop.weatherappattempt2.databinding.FragmentHoursBinding
 import ru.startandroid.develop.weatherappattempt2.databinding.FragmentMainBinding
 
@@ -19,17 +21,33 @@ class HoursFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+        // return inflater.inflate(R.layout.fragment_hours, container, false) я не помню откуда и что это, но удалять на всякий случай не буду
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRcView()
     }
 
     private fun InitRcView() = with(binding) { //с помощью этого мы получаем доступ к элементам экрана
-        rcView //ошибка хз че делать
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+
+            WeatherModel("", "12:00", "Sunny", "25°C", "", "", "", "")
+            //WeatherModel("", "13:00", "Sunny", "25°C", "", "", "", "")
+            //WeatherModel("", "14:00", "Sunny", "25°C", "", "", "", "")
+
+
+
+
+
+        )
+        adapter.submitList() //загружаем список
     }
 
 
