4f2915bae28ed2ebe409dd03008b0c68d881402d
Ksenia <ksyshishe@mail.ru>
2023-08-04 23:30:39 +0300

    clinning of comments plus some mistake

--- app/src/main/java/ru/startandroid/develop/weatherappattempt2/adapters/WeatherAdapter.kt
+++ app/src/main/java/ru/startandroid/develop/weatherappattempt2/adapters/WeatherAdapter.kt
@@ -36,7 +36,7 @@ class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder> (Compara
 
 
     }
-//что-то
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder { //метод 1 - создает эелемент
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false) //создается вью
         return Holder(view)//создается холдер
